10fd4ebd9332ed0ba72cf2dc3e518dd62909b8e7
youcef-chihi <youcef.chihi@gmail.com>
2018-12-29 18:32:50 +0100

    we rollin yet?

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require ('express');
 const Jimp = require ('jimp');
 const app = express ();
-const port = 8080;
+const port = process.env.PORT || 8080;
 
 
 const randomBetween = (a, b) => a < b ? Math.floor(Math.random() * b) + a : Math.floor(Math.random() * a) + b  
